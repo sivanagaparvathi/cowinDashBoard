9a6a373e18428b42c1878076332163f0c7300bb2
sivanagaparvathi <130878997+sivanagaparvathi@users.noreply.github.com>
2023-07-26 15:35:39 +0530

    Update index.js

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -5,7 +5,7 @@ import {
   XAxis,
   YAxis,
   Legend,
-  ResponsiveContainer,
+ 
 } from 'recharts'
 
 import './index.css'
@@ -21,8 +21,10 @@ const VaccinationCoverage = props => {
   return (
     <div className="coverage-card-container">
       <h1 className="coverage-card-heading">Vaccination Coverage</h1>
-      <ResponsiveContainer width={1000} height={300}>
+      
         <BarChart
+          width={900}
+          height={400}
           data={vaccinationData}
           margin={{
             top: 5,
@@ -50,7 +52,7 @@ const VaccinationCoverage = props => {
           <Bar dataKey="dose_1" name="Dose 1" fill="#5a8dee" barSize="20%" />
           <Bar dataKey="dose_2" name="Dose 2" fill="#f54394" barSize="20%" />
         </BarChart>
-      </ResponsiveContainer>
+      
     </div>
   )
 }
